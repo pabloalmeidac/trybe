24740c141f0e98ab98676609fb1904d169ce5367
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-27 03:23:08 -0300

    Feat: add func exec input

--- back-end/bloco-26-typescript/introducao/dist/area.js
+++ back-end/bloco-26-typescript/introducao/dist/area.js
@@ -1,10 +1,42 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
 const unitys = ['km²', 'hm²', 'dam²', 'm²', 'dm²', 'cm²', 'mm²'];
+const errorValue = 'Valor não pode ser menor ou igual a 0';
+const errorUnity = 'Digite entre 0 a 6 para escolher unidade base';
+const unity = '0 - km² | 1 - hm² | 2 - dam² | 3 - m² | 4 - dm² | 5 - cm² | 6 - mm²';
+const exec = () => {
+    const value = readline_sync_1.default.questionFloat('Digite o valor para ser convertido \n');
+    if (value >= 0) {
+        const unityBase = readline_sync_1.default.questionInt(`Digite o numero correspondente a unidade base do valor escolhido \n${unity}\n`);
+        if (unityBase >= 0 && unityBase <= 6) {
+            const forConvert = unitys[unityBase];
+            const unityConversion = readline_sync_1.default.questionInt(`Digite o numero correspondente a unidade a ser convertida \n${unity}\n`);
+            if (unityConversion >= 0 && unityConversion <= 6) {
+                const toConvert = unitys[unityConversion];
+                return convert(value, forConvert, toConvert);
+            }
+            else {
+                return errorUnity;
+            }
+        }
+        else {
+            return errorUnity;
+        }
+    }
+    else {
+        return errorValue;
+    }
+};
 const convert = (value, unityBase, unityConversion) => {
     const forIndex = unitys.indexOf(unityBase);
     const toIndex = unitys.indexOf(unityConversion);
     const exponent = (toIndex - forIndex);
-    return value * Math.pow(10, exponent);
+    const result = value * Math.pow(10, exponent);
+    console.log(`A conversão de ${value}${unityBase} é ${result}${unityConversion}`);
+    return result;
 };
-exports.default = convert;
+exports.default = exec;
--- back-end/bloco-26-typescript/introducao/mass.ts
+++ back-end/bloco-26-typescript/introducao/mass.ts
@@ -1,11 +1,45 @@
+import readline from "readline-sync";
+
 const unitys: string[] = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
+const errorValue: string = 'Valor não pode ser menor ou igual a 0';
+const errorUnity: string = 'Digite entre 0 a 6 para escolher unidade base';
+const unity: string = '0 - kg | 1 - hg | 2 - dag | 3 - g | 4 - dg | 5 - cg | 6 - mg';
+
+const exec = (): number | string => {
+  const value = readline.questionFloat('Digite o valor para ser convertido \n');
+  
+  if (value >= 0) {
+    const unityBase = readline.questionInt(`Digite o numero correspondente a unidade base do valor escolhido \n${unity}\n`);
+
+    if (unityBase >= 0 && unityBase <= 6) {
+      const forConvert: string = unitys[unityBase];
+
+      const unityConversion = readline.questionInt(`Digite o numero correspondente a unidade a ser convertida \n${unity}\n`);
+
+      if (unityConversion >= 0 && unityConversion <= 6) {
+        const toConvert: string = unitys[unityConversion]; 
+
+        return convert(value, forConvert, toConvert);
+      } else {
+        return errorUnity;
+      }
+    } else {
+      return errorUnity;
+    }
+  } else {
+    return errorValue;
+  }
+}
 
 const convert = (value: number, unityBase: string, unityConversion: string): number => {
   const forIndex: number = unitys.indexOf(unityBase);
   const toIndex: number = unitys.indexOf(unityConversion); 
   const exponent: number = (toIndex - forIndex);
 
-  return value * Math.pow(10, exponent);
+  const result = value * Math.pow(10, exponent);
+  console.log(`A conversão de ${value}${unityBase} é ${result}${unityConversion}`);
+  
+  return result;
 }
 
-export default convert;
+export default exec;
