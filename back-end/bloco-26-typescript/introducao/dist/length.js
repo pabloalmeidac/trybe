be2ae2d86477390df63197b278794a249e829cbd
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-27 00:55:54 -0300

    Feat: refactor

--- back-end/bloco-26-typescript/introducao/dist/length.js
+++ back-end/bloco-26-typescript/introducao/dist/length.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.convert = void 0;
 const unitys = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 const convert = (value, unityBase, unityConversion) => {
     const forIndex = unitys.indexOf(unityBase);
@@ -8,4 +7,4 @@ const convert = (value, unityBase, unityConversion) => {
     const exponent = (toIndex - forIndex);
     return value * Math.pow(10, exponent);
 };
-exports.convert = convert;
+exports.default = convert;
