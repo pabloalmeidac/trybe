c9695e696219987272b668981465cc54e6a1b63d
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-27 00:55:41 -0300

    Feat: refactor

--- back-end/bloco-26-typescript/introducao/length.ts
+++ back-end/bloco-26-typescript/introducao/length.ts
@@ -1,9 +1,11 @@
 const unitys: string[] = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 
-export const convert = (value: number, unityBase: string, unityConversion: string): number => {
+const convert = (value: number, unityBase: string, unityConversion: string): number => {
   const forIndex: number = unitys.indexOf(unityBase);
   const toIndex: number = unitys.indexOf(unityConversion); 
   const exponent: number = (toIndex - forIndex);
 
   return value * Math.pow(10, exponent);
 }
+
+export default convert;
