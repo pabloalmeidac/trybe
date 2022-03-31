8ea15d691bddfe91edb4a58750075b865adb007a
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-30 22:14:04 -0300

    FEAT: Crud user done

--- back-end/bloco-26-typescript/express-com-typescript/src/App.ts
+++ back-end/bloco-26-typescript/express-com-typescript/src/App.ts
@@ -1,12 +1,34 @@
+import * as dotenv from "dotenv";
 import express, { Express, NextFunction, Response, Request } from 'express';
+import UserController from "./controller/User.controller";
+import UserModel from "./model/User.model";
+import UserService from "./service/User.service";
+import Connection from './model/connection';
+dotenv.config();
 
 export default class App {
   private app: Express;
+  private PORT = process.env.PORT;
+
+  private connection: Connection
+  private userControler: UserController;
+  private userService: UserService;
+  private userModel: UserModel;
+
+
 
   constructor() {
     this.app = express();
 
     this.app.use(express.json());
+
+    this.connection = new Connection();
+
+    this.userModel = new UserModel(this.connection);
+
+    this.userService = new UserService(this.userModel);
+
+    this.userControler = new UserController(this.userService);
 
     this.routes();
   }
@@ -15,12 +37,17 @@ export default class App {
     this.app.get(
       '/',
       (req: Request, res: Response, next: NextFunction) => {
-        res.status(200).json({ message: 'Acessou' });
-      }
+        res.status(200).json({message: 'Acessou.'})
+    },
     )
+    this.app.get('/users', this.userControler.getAll);
+    this.app.get('/users/:id', this.userControler.getById);
+    this.app.post('/users', this.userControler.create);
+    this.app.put('/users/:id', this.userControler.update);
+    this.app.delete('/users/:id', this.userControler.exclude);
   }
 
   public start() {
-    this.app.listen(3000, () => console.log('Ouvindo na porta 3000'));
+    this.app.listen(this.PORT, () => console.log(`Ouvindo na porta ${this.PORT}`));
   }
 }
---
+++ back-end/bloco-26-typescript/express-com-typescript/src/controller/User.controller.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import UserService from "../service/User.service";
+
+export default class UserController {
+
+  constructor(private userService: UserService) {}
+
+  getAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.userService.getAll();
+
+      return res.status(200).json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  getById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const user = await this.userService.getById(id);
+
+      return res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    try {
+      const { status, data } = await this.userService.create({ name, email, password });
+
+      return res.status(status).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  update = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    const { id } = req.params;
+    try {
+      const { status, data } = await this.userService.update(id, { name, email, password });
+
+      return res.status(status).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  exclude = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const { status, data } = await this.userService.exclude(id);
+
+      return res.status(status).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
